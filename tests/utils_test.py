63618f46a7a4b046658cd8eb17d526b11e3369a6
Sung-Kyu Yoo <skyoo2003@gmail.com>
2021-04-26 15:22:20 +0900

    Add to_kst unit test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,4 +1,7 @@
-from kakaowork.utils import text2dict, exist_kv
+from datetime import datetime
+
+from kakaowork.consts import KST
+from kakaowork.utils import text2dict, exist_kv, to_kst
 
 
 def test_text2dict():
@@ -14,3 +17,8 @@ def test_exist_kv():
     assert exist_kv('key', {"key": None}) is False
     assert exist_kv('key', {"key": ""}) is False
     assert exist_kv('key', {"key": "value"}) is True
+
+
+def test_to_kst():
+    assert to_kst(1617889170) == KST.localize(datetime(2021, 4, 8, 22, 39, 30))
+    assert to_kst(1609426800) == KST.localize(datetime(2021, 1, 1, 0, 0, 0))
