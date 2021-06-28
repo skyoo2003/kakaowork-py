f7c895d78ffba2ac935c673f12df6d85d9b57c75
Sung-Kyu Yoo <skyoo2003@gmail.com>
2021-06-28 10:15:21 +0900

    Remove unnecessary code

--- kakaowork/__main__.py
+++ kakaowork/__main__.py
@@ -3,8 +3,8 @@ import sys
 
 def main():  # noqa: D103
     try:
-        from kakaowork.command import cli, _CLIOptions
-        cli(obj=_CLIOptions())
+        from kakaowork.command import cli
+        cli()
     except ImportError as e:
         sys.exit(f'Does not support CLI: ({e})')
 
